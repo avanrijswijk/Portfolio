88dbe62f4a7d82f71fbda64fadb966567a08d589
Adrian VAN RIJSWIJK <adrianlvr127@gmail.com>
2026-04-14 19:05:47 +0200

    mise à jour des informations

--- animations.js
+++ animations.js
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", function() {
     if (document.getElementById('typed-text')) {
         var typed = new Typed('#typed-text', {
             strings: [
-                "Étudiant en 2ème année de BUT Informatique.",
-                "Passionné par le développement logiciel.",
-                "À la recherche d'un stage pour mettre en pratique mes acquis."
+                "Étudiant en 3ème année de BUT Informatique.",
+                "Passionné par le développement web et logiciel.",
+                "À la recherche d'une alternance pour mettre en pratique mes acquis."
             ],
             typeSpeed: 40,
             backSpeed: 30,
